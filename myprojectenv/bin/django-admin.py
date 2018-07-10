c97df94a1742dcc7f6affda5c3779822bf185591
Yoohogyun <yoohoogun114@naver.com>
2018-07-10 13:50:00 +0900

    first commit

---
+++ myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home1/irteam/naver/search-support/toy/myproject/myprojectenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
